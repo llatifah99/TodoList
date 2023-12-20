7700b2197962ae657d1c1181b8a42920b345e7c3
llatifah99 <litalatifah6@gmail.com>
2023-12-20 23:26:26 +0700

    membuat file TodoApp yang isinya judul, input dan button

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,10 @@
+const TodoApp = () => {
+  return (
+    <>
+      <h3>TodoList:</h3>
+      <input type="text" placeholder="Add a task"></input>
+      <button>Add</button>
+    </>
+  );
+};
+export default TodoApp;
