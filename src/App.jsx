b2d7df82f77f9ac88cc362486132eb5558cccdea
llatifah99 <litalatifah6@gmail.com>
2023-12-20 23:30:10 +0700

    import TodoApp ke App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import TodoApp from "./components/TodoApp";
 
 function App() {
   return (
     <>
-      <h1>Tes React</h1>
+      <TodoApp />
     </>
   );
 }
