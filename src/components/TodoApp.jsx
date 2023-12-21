df444504f4aebb6c9e8051adf16de659fde78745
llatifah99 <litalatifah6@gmail.com>
2023-12-21 16:57:59 +0700

    main.js dan TodoApp

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -2,11 +2,26 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setTask, addTask } from "../redux/Slice/todoSlice";
 const TodoApp = () => {
+  const dispatch = useDispatch();
+  const { task, tasks } = useSelector((state) => state.todo);
+  const handleInputChange = (e) => {
+    dispatch(setTask(e.target.value));
+  };
+  const handleAddTask = () => {
+    dispatch(addTask());
+  };
   return (
     <>
       <h3>TodoList:</h3>
-      <input type="text" placeholder="Add a task"></input>
-      <button>Add</button>
+      <input
+        type="text"
+        value={task}
+        onChange={handleInputChange}
+        placeholder="Add a task"
+      />
+      <button onClick={handleAddTask} className="btn">
+        Add
+      </button>
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import TodoApp from "./components/TodoApp";
+import "./App.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.render(
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>,
+  document.getElementById("root")
+);
